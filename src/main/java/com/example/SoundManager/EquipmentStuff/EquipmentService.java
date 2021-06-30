44ffe41fe361fd582ca7f3eef80d9dd6b77d7be7
LatheWard <lward21@basecampcodingacademy.org>
2021-06-30 13:43:11 -0500

    Added delete request and added it to all templates

--- src/main/java/com/example/SoundManager/EquipmentStuff/EquipmentService.java
+++ src/main/java/com/example/SoundManager/EquipmentStuff/EquipmentService.java
@@ -7,7 +7,12 @@ import org.springframework.stereotype.Service;
 public class EquipmentService {
     @Autowired
     private EquipmentRepository equipRepo;
-    public void saveEquipment(Equipment equipment){
+
+    public void saveEquipment(Equipment equipment) {
         equipRepo.save(equipment);
+    }
+
+    public void delete(long id) {
+        equipRepo.deleteById(id);
     }
 }
